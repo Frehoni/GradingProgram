54f07cf7913175b56eced0f979acfebeb737fba7
Frehoni <frehoni@gmail.com>
2019-11-21 17:22:26 +0100

    DUMPE POLITIK

--- dataLoad.py
+++ dataLoad.py
@@ -13,14 +13,35 @@ def dataLoad(filename):
         #print(fildata)
     except FileNotFoundError:
         return print("Date file not found, please try again")
+    
+    
     data = np.array(fildata)
     studentid =np.array(fildata.StudentID)
     repeats = np.array([item for item, count in Counter(studentid).items() if count > 1]) #https://stackoverflow.com/a/11528581
     for a in range(np.size(repeats)):
         print("The StudentID {} is stated more than once in the datafile.".format(repeats[a]))
-    for b in range(np.size(studentid)):
-        grade = np.array(fildata.iloc[b,2:])
-        grades = np.append(grades,(np.sum(grade)/np.size(grade))) #np.mean gav os ingen decimaler, så vi gjorde det som en flok hulemennesker ville.
+    
+    
+    
+    
+    
+    for b in range(np.size(studentid)):#Calculating Average
+        if -3 in fildata.iloc[b,2:]:
+            grades = np.append(grades,-3)
+        else:
+            if np.size(np.array(fildata.iloc[b,2:])) == 1:
+                grades = np.append(grades,fildata.iloc[b,2:])
+            if np.size(np.array(fildata.iloc[b,2:])) > 1:
+                grade = np.array(fildata.iloc[b,2:])
+                grade = np.delete(grade,grade.argmin())
+                grades = np.append(grades,(np.sum(grade)/np.size(grade))) #np.mean gav os ingen decimaler, så vi gjorde det som en flok hulemennesker ville.
+            if np.size(np.array(fildata.iloc[b,2:])) == 0:
+                print("There are no grades!")
+       
+        
+        
+        
+        
     for c in range(np.size(fildata.iloc[0,:])-2):
         assignments = np.append(assignments,'Assignment {}'.format(c+1))
     numbers = np.arange(1,np.size(studentid)+1)
@@ -28,6 +49,7 @@ def dataLoad(filename):
     finaldata = np.c_[data,grades]
     df = pd.DataFrame(finaldata,columns=assignments,index=numbers)
     df_sort = df.sort_values('Name')
+    print(fildata)
     print(df_sort)
     
     return (grades)
